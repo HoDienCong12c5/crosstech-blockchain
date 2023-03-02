a5a3c99eff45ba93d5cfe919363d480395011c59
cong@bacoor <cong@bacoor>
2023-03-02 20:45:22 +0700

    setup

--- pages/Container/Header/index.js
+++ pages/Container/Header/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { ContainerHeader } from './styled'
+import { Col, Row } from 'antd'
+import ButtonBasic from '@/Components/ButtonBasic/index';
+import styles from './Header.module.scss'
 const Header = () => {
   return (
-    <div>Header</div>
+    <ContainerHeader>
+      <Row justify={'space-between'}>
+        <Col span={4} style={{alignContent:'start'}}>
+          <fiv>logo</fiv>
+        </Col>
+        <Col span={16}>
+          <fiv>logo</fiv>
+        </Col>
+        <Col span={4} style={{alignContent:'end'}}>
+          <ButtonBasic style={styles['btn-login']} >
+            Login
+          </ButtonBasic>
+        </Col>
+      </Row>
+    </ContainerHeader>
   )
 }
 
---
+++ pages/Container/Header/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+export const ContainerHeader = styled.div`
+    width: 100%
+    max-width:1550px
+    height: 50px;
+    text-align:center;
+    margin: auto;
+`;
+export default ()=>{};
