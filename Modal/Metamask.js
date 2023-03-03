a40ca413e56df42e1fc8540a25480b4a4a7f5b64
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-03 10:01:45 +0700

    [website] sign metamask

--- Modal/Metamask.js
+++ Modal/Metamask.js
@@ -1,13 +1,103 @@
-import ReduxService from '@/Utils/ReduxService';
+import MetaMaskOnboarding from '@metamask/onboarding';
+import { isMobile } from 'react-device-detect';
+import Web3 from 'web3';
+
 const { ethereum } = typeof window !== 'undefined' ? window : {};
+let onboarding
 class Metamask{
   static async connect(){
     if ( typeof window.ethereum !== 'undefined' ) {
-      const metaMask = await ethereum.request( { method: 'eth_accounts' } )
-      console.log( {metaMask} );
-      ReduxService.setMetamask( metaMask );
+      // const metaMask = await ethereum.request( { method: 'eth_accounts' } )
+      // console.log( {metaMask} );
+      // ReduxService.setMetamask( metaMask );
+      this.initialize()
     }else{
-        console.log( 'not insstalled' );
+      console.log( 'not insstalled' );
+    }
+  }
+  static async initialize () {
+    try {
+      if ( !onboarding ) {
+        onboarding = new MetaMaskOnboarding()
+      }
+      if ( !MetaMaskOnboarding.isMetaMaskInstalled() && !isMobile ) {
+        onboarding.startOnboarding()
+      } else {
+        onboarding.stopOnboarding()
+        let accounts = await window.ethereum.request( {
+          method: 'eth_requestAccounts'
+        } )
+        console.log( '====================================' );
+        console.log( {accounts} );
+        console.log( '====================================' );
+        if ( accounts.length > 0 ) {
+          this.onConnect( accounts )
+          this.signPersonalMessage( accounts[0] )
+          // subscribe to events
+          this.subscribeToEvents()
+        } else {
+          let accounts = await this.enableMetaMask()
+          this.onConnect( accounts )
+          // subscribe to events
+          this.subscribeToEvents()
+        }
+      }
+    } catch ( error ) {
+      console.log( '====================================' );
+      console.log( {onboarding} );
+      console.log( '====================================' );
+      console.log( 'initialize', error )
+    }
+  }
+  static subscribeToEvents () {
+    if ( MetaMaskOnboarding.isMetaMaskInstalled() ) {
+      // window.ethereum.on( 'chainChanged', this.handleNewChain )
+      // window.ethereum.on( 'networkChanged', this.handleNewNetwork )
+      // window.ethereum.on( 'accountsChanged', this.handleNewAccounts )
+    }
+  }
+  static async onConnect ( accounts ) {
+    // const address = accounts[0]
+    // const callbackSignIn = () => {
+    //   Observer.emit( OBSERVER_KEY.ALREADY_SIGNED )
+    // }
+    // await this.getNetworkAndChainId()
+    // // update redux state
+    // await ReduxService.updateMetaMask( {
+    //   accounts,
+    //   address
+    // } )
+    // ReduxService.loginMetamask( callbackSignIn )
+  }
+
+  static signPersonalMessage ( address, message ) {
+    message = 'CrossTech'
+    console.log( '====================================' );
+    console.log( {msgParams} );
+    console.log( '====================================' );
+    const msgParams = [
+      Web3.utils.toHex( message ),
+      address
+    ]
+    console.log( '====================================' );
+    console.log( {msgParams} );
+    console.log( '====================================' );
+    if ( window.ethereum ) {
+      return new Promise( ( resolve, reject ) => {
+        // Sign transaction
+        window.ethereum
+          .request( { method: 'personal_sign', params: msgParams } )
+          .then( ( result ) => {
+            // Returns signed transaction
+            return resolve( result )
+          } )
+          .catch( ( error ) => {
+            // Error returned when rejected
+            return reject( error )
+          } )
+      } )
+    } else {
+      return null
     }
   }
 }
