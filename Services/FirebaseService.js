2f37dd281beaf8846711e686e0f3c33b165b2c93
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-12 17:14:22 +0700

    [webiste] fix-ui

--- Services/FirebaseService.js
+++ Services/FirebaseService.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_FiB,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID,
   appId: process.env.NEXT_PUBLIC_APPID,
--- pages/Screen/Home/index.js
+++ pages/Screen/Home/index.js
@@ -1,6 +1,5 @@
 import ItemNFT from '@/Components/ItemNFT';
 import useGetNFT from '@/Hook/useGetNFT';
-import IPFSService from '@/Services/IPFSService';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Media from 'react-media';
@@ -26,38 +25,20 @@ const menuHome = [
   }
 
 ]
-let dataFake = []
-for (let i = 0; i < 10; i++) {
-  dataFake.push({
-    time: '12/05/2000',
-    hash: '0xa99ea7dfb13cce5a604bf61ef2de521de279f5fc18d75e172e040de70c46c3dd ',
-    form: '0xbB47BDD15Aee646b66c03b8cCd1AD1C2AfE5d72c',
-    to: '0xbB47BDD15Aee646b66c03b8cCd1AD1C2AfE5d72c',
-    contract: '0xbB47BDD15Aee646b66c03b8cCd1AD1C2AfE5d72c',
-    data: {
-      image: 'https://ipfs.pantograph.app/ipfs/QmaKHqMGLJVrTnopyLbE2cuvs4erCdvtTjzPWZE18PXxjj?filename=Guardian_AQUA.png',
-      address: '0xbB47BDD15Aee646b66c03b8cCd1AD1C2AfE5d72c',
-      nameUser: 'Ho Dien Cong',
-      title: 'Basic blockchain',
-    }
-  })
-}
-
 const HomeScreen = () => {
   const router = useRouter()
-
   const [itemSelected, setItemSelected] = useState('qa_qc')
   const [listAllNFTs, setListAllNFTs] = useState([])
   const { listNFTAll, loading } = useGetNFT()
   const onClickItemMenu = (key) => {
     setItemSelected(key)
     switch (key) {
-      case 'qa_qc':
+    case 'qa_qc':
 
-        break;
+      break;
 
-      default:
-        break;
+    default:
+      break;
     }
   }
   useEffect(() => {
@@ -65,22 +46,6 @@ const HomeScreen = () => {
       setListAllNFTs([...listNFTAll, ...listNFTAll, ...listNFTAll])
     }
   }, [listNFTAll])
-  console.log('====================================');
-  console.log({ listAllNFTs });
-  console.log('====================================');
-  const loadFile = async (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const files = (form[0]).files;
-    if (!files || files.length === 0) {
-      return alert('No files selected');
-    }
-    const file = files[0];
-    console.log({ file });
-    const path = await IPFSService.uploadFile(file)
-    console.log({ path })
-
-  }
   const renderDesktop = () => {
     return (
       <ContentHome>
