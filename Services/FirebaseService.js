f3b5edaf9f39f6c02d889dee9881434ddf8fc869
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-12 19:31:17 +0700

    [webiste] fix-ui

--- Services/FirebaseService.js
+++ Services/FirebaseService.js
@@ -11,16 +11,26 @@ import functionStore from './functionStore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+//   databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID_FB,
+//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_APPID,
+//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+// }
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_FB,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APPID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-}
+  apiKey: 'AIzaSyCXFxZWd18pNxsgbQaVUcL575HIcXez5Fo',
+  authDomain: 'bth4-d5049.firebaseapp.com',
+  databaseURL: 'https://bth4-d5049-default-rtdb.firebaseio.com',
+  projectId: 'bth4-d5049',
+  storageBucket: 'bth4-d5049.appspot.com',
+  messagingSenderId: '24144431299',
+  appId: '1:24144431299:web:9209482646946ee5edba76',
+  measurementId: 'G-R7EXMD6CDK'
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 setLogLevel('debug')
