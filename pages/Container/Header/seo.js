ba91bf762c1582234dcadecbb7e873286e399c2d
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-12 16:54:43 +0700

    [webiste] fix-ui

--- pages/Container/Header/seo.js
+++ pages/Container/Header/seo.js
@@ -1,37 +1,60 @@
+import { DefaultSeo, NextSeo } from 'next-seo';
 import Head from 'next/head'
 import React from 'react'
-
+const SEO_param = {
+  title: 'Cross - Tech',
+  description: 'Cross - Tech basic blockchain',
+  openGraph: {
+    url: 'https://crosstech-blockchain.vercel.app/',
+    title: 'Cross - Tech',
+    description: 'Cross - Tech basic blockchain',
+    images: [
+      {
+        url: 'http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM',
+        width: 800,
+        height: 600,
+        alt: 'Image Alt Text',
+      },
+    ],
+    site_name: 'Cross - Tech',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
 const SEO = () => {
   return (
-    <Head>
-      <meta charSet="utf-8" />
-      <title>Cross - Tech</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM" />
-      <meta name="description" content="Eatnsmile"/>
+    <>
+      <DefaultSeo {...SEO_param} />
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Cross - Tech</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://skywalker.infura-ipfs.io/ipfs/QmTZBqX5FbMPQEC6AaTmhaqDSPv3gpp5hb3XzZN7piA534" />
+        <meta name="description" content="Eatnsmile"/>
 
-      {/* <!-- Google / Search Engine Tags --/> */}
-      <meta itemprop="name" content="Cross - Tech"/>
-      <meta itemprop="description" content="Cross - Tech"/>
-      <meta itemprop="image" content="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM"/>
 
-      {/* <!-- Facebook Meta Tags --/> */}
-      <meta property="og:url" content="http://dev-star-token-client.w3w.app"/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:title" content="Cross - Tech"/>
-      <meta property="og:description" content="Cross - Tech"/>
-      <meta property="og:image" content="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM"/>
+        <meta itemprop="name" content="Cross - Tech"/>
+        <meta itemprop="description" content="Cross - Tech"/>
+        <meta itemprop="image" content="https://skywalker.infura-ipfs.io/ipfs/QmTZBqX5FbMPQEC6AaTmhaqDSPv3gpp5hb3XzZN7piA534"/>
 
-      {/* <!-- Twitter Meta Tags --> */}
-      <meta name="twitter:card" content="summary_large_image"/>
-      <meta name="twitter:title" content="Cross - Tech"/>
-      <meta name="twitter:description" content="Cross - Tech"/>
-      <meta name="twitter:image" content="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM" />
+        <meta property="og:url" content="http://dev-star-token-client.w3w.app"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Cross - Tech"/>
+        <meta property="og:description" content="Cross - Tech"/>
+        <meta property="og:image" content="https://skywalker.infura-ipfs.io/ipfs/QmTZBqX5FbMPQEC6AaTmhaqDSPv3gpp5hb3XzZN7piA534"/>
 
-    </Head>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="Cross - Tech"/>
+        <meta name="twitter:description" content="Cross - Tech"/>
+        <meta name="twitter:image" content="https://skywalker.infura-ipfs.io/ipfs/QmTZBqX5FbMPQEC6AaTmhaqDSPv3gpp5hb3XzZN7piA534" />
+
+      </Head>
+    </>
+
   )
 }
-export async function getInitialProps ({ params, res }) {
-  return {}
-}
+
 export default SEO
