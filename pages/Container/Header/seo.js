1cb412e80ea5b1871db39fad83ec9be285c7011a
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-12 16:32:21 +0700

    [webiste] fix-ui

--- pages/Container/Header/seo.js
+++ pages/Container/Header/seo.js
@@ -7,26 +7,26 @@ const SEO = () => {
       <meta charSet="utf-8" />
       <title>Cross - Tech</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="https://skywalker.infura-ipfs.io/ipfs/QmXtHZwGGsvvMmVBJDCjDH4fGNPa9rKw5zaSfkX9iSVc2r" />
+      <link rel="icon" href="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM" />
       <meta name="description" content="Eatnsmile"/>
 
       {/* <!-- Google / Search Engine Tags --/> */}
       <meta itemprop="name" content="Cross - Tech"/>
       <meta itemprop="description" content="Cross - Tech"/>
-      <meta itemprop="image" content="https://skywalker.infura-ipfs.io/ipfs/QmXtHZwGGsvvMmVBJDCjDH4fGNPa9rKw5zaSfkX9iSVc2r"/>
+      <meta itemprop="image" content="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM"/>
 
       {/* <!-- Facebook Meta Tags --/> */}
       <meta property="og:url" content="http://dev-star-token-client.w3w.app"/>
       <meta property="og:type" content="website"/>
       <meta property="og:title" content="Cross - Tech"/>
       <meta property="og:description" content="Cross - Tech"/>
-      <meta property="og:image" content="https://skywalker.infura-ipfs.io/ipfs/QmXtHZwGGsvvMmVBJDCjDH4fGNPa9rKw5zaSfkX9iSVc2r"/>
+      <meta property="og:image" content="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM"/>
 
       {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:title" content="Cross - Tech"/>
       <meta name="twitter:description" content="Cross - Tech"/>
-      <meta name="twitter:image" content="https://skywalker.infura-ipfs.io/ipfs/QmXtHZwGGsvvMmVBJDCjDH4fGNPa9rKw5zaSfkX9iSVc2r" />
+      <meta name="twitter:image" content="http://ipfs.pantograph.app/ipfs/QmXn99vcRhrYdcZDQyunKPuwPABA2neYne6XLcVBLxrjTM" />
 
     </Head>
   )
