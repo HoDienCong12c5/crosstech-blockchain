ed04ddc5bfb69471d47e2a3578a94e7d5b60331c
Hồ Diên Thành <74367651+HoDIenThanh12@users.noreply.github.com>
2023-03-21 22:02:54 +0700

    [website] add index-gg

--- pages/Screen/LandingPage/index.js
+++ pages/Screen/LandingPage/index.js
@@ -6,7 +6,6 @@ import Img, { images } from '@/common/images'
 import { Col } from 'antd'
 import styles from './LP.module.scss'
 import BannerLP from './Components/Banner'
-import Head from 'next/head'
 const LandingPage = () => {
 
 
@@ -28,39 +27,20 @@ const LandingPage = () => {
 
 
   return (
-    <>
-      <Head >
-        {/* <!-- Google Tag Manager --> */}
-        <script dangerouslySetInnerHTML={{
-          __html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WRGMC62');`
-        }}/>
-        {/* <!-- End Google Tag Manager --> */}
-      </Head>
-      {/* <!-- Google Tag Manager (noscript) --> */}
-      <noscript dangerouslySetInnerHTML={{
-        __html:`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRGMC62"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-      }}/>
-      {/* <!-- End Google Tag Manager (noscript) --> */}
-      <ContainerLP className='container-basic'>
-        <SEOLP />
-        <BannerLP />
+    <ContainerLP className='container-basic'>
+      <SEOLP />
+      <BannerLP />
 
-        <Media query='(min-width: 768px)'>
-          {(match) => {
-            if (match) {
-              return renderDesktop()
-            }
-            return renderMobile()
-          }}
+      <Media query='(min-width: 768px)'>
+        {(match) => {
+          if (match) {
+            return renderDesktop()
+          }
+          return renderMobile()
+        }}
 
-        </Media>
-      </ContainerLP>
-    </>
+      </Media>
+    </ContainerLP>
   )
 }
 
