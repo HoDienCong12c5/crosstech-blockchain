ccc91c373f13efedbd70743239015ba119cb45b4
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-22 08:55:11 +0700

    [website] fix-ui

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head >
         <meta name="robots" content="all" />
-        <meta name="googlebot" content="noindex,nofollow" />
+        <meta name="googlebot" content="all" />
         <link rel="icon" href="https://skywalker.infura-ipfs.io/ipfs/QmfSbEq4qrQn53YydFj59Saiz9issKWFuxEJS4hDTCnNzh" />
         {/* <!-- Google Tag Manager --> */}
         <script dangerouslySetInnerHTML={{
